8832ea7598f2f37c64b871e843a25164f2af1fc7
NikuDubenco <nicolae.dubenco@gmail.com>
2019-07-26 14:20:13 +0100

    create models.py and linked to DB

--- twityoff/app.py
+++ twityoff/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from .models import DB
 
 def create_app():
 	'''Create and configurate an instance of the Flask application'''
 	app = Flask(__name__)
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+	DB.init_app(app)
 
 	@app.route('/')
 	def root():
